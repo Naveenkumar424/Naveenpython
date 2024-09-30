63124399bc7298621d83e4fad7aa248b81478f7c
Naveen <naveenkulaljs2@gmail.com>
2024-09-30 06:04:34 +0530

    modified a few lines

--- B7.py
+++ B7.py
@@ -3,5 +3,5 @@ from numpy import *
 a = array([[1,4,3],[4,3,1]])
 b = array([[8,4,0],[1,2,3]])
 print("Sum of two matrices are:\n",a+b)
-print("difference of two matrices are:\n",a-b)
+print("difference of two matrices are:\n",a- b)
 #end of code
